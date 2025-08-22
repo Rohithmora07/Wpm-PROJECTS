e9d801ebddfea69b5dc2ec016b23ee0e204419dc
Mora Rohith <23eg106b42@anurag.edu.in>
2025-08-22 10:57:15 +0530

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  res.render('index', { title: 'Express Backend Server' });
+  res.render('index', { title: 'SHARECARE' });
 });
 
 module.exports = router;
